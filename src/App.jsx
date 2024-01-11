be28758db0fa803c98561f09ba97382ec53ca942
Akbarof01 <akhbbarov01@gmail.com>
2024-01-11 15:21:35 +0500

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const removeAllTasks = () => {
+    setTasks([]);
+  };
+
+  return (
+    <div className='todo'>
+      <h1>To-Do App</h1>
+      <div className='todo1'>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      {tasks.length > 0 && (
+        <button onClick={removeAllTasks}>Delete All</button>
+      )}
+    </div>
+  );
+};
+
+export default TodoApp;
